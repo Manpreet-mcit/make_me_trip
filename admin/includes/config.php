933df42c47c2816bf57bf630b22f58fb7fdfd2dc
Manpreet <manpreet.7385@gmail.com>
2022-03-12 16:16:06 +0530

    pushing the final changes

---
+++ admin/includes/config.php
@@ -0,0 +1,16 @@
+<?php 
+// DB credentials.
+define('DB_HOST','bvslcqkwvbklzvumvlqq-mysql.services.clever-cloud.com');
+define('DB_USER','upekkkczu1avfv83');
+define('DB_PASS','nCVU3kMp4rj220Cppkkc');
+define('DB_NAME','bvslcqkwvbklzvumvlqq');
+// Establish database connection.
+try
+{
+$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+}
+catch (PDOException $e)
+{
+exit("Error: " . $e->getMessage());
+}
+?>
